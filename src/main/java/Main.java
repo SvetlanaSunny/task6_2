ae7dc28b76c0774a26a5abe34c76ec73bbb26738
Светлана Чернова <lana_19.87@mail.ru>
2022-03-24 15:10:40 +0600

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+       BonusService service = new BonusService();
+        // подготавливаем данные:
+       long amount = 1000_60; // внесенная сумма
+       boolean registred = true; //  пользователь зарегестрирован
+       long expected = 30; // ожидаемый результат
+        // вызываем целевой метод:
+        long actual = service.calculate(amount,registred); // фактический результат
+        // // производим проверку (сравниваем ожидаемый и фактический):
+        boolean passed = expected == actual;
+
+
+       System.out.println(service.calculate(10000, true));
+    }
+}
